531f47898bf324312c5310634c931af2a9740b70
fabian222222 <fabian.zuo.dev@outlook.fr>
2022-01-25 10:31:07 +0100

    about me section

---
+++ src/Components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div>
+            <h1>
+                Fabian.is()
+            </h1>
+            <div>
+                <div>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae 
+                    quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+                </div>
+                <div>
+                    <p>Nom :</p>
+                    <p>Mail :</p>
+                    <p>Localisation :</p>
+                    <p>Téléphone :</p>
+                </div>
+            </div>
+            <button>CV download</button>
+        </div>
+    );
+};
+
+export default About;
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styled from 'styled-components';
+import Typed from "typed.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import './../style/haha.css'
 
 const Landing = () => {
+    const description = useRef(null)
+
+    useEffect(()=>{
+        const typed = new Typed(description.current, {
+            strings:["développeur", "junior", "haha"],
+            startDelay: 300,
+            typeSpeed:100,
+            backSpeed:30,
+            backDelay:500,
+            loop:true,
+            showCursor:true,
+            cursorChar:"/"
+        })
+
+        return () => {
+            typed.destroy()
+        }
+    }, [])
+
     return(
         <Container>
             <InformationContainer>
@@ -12,9 +35,25 @@ const Landing = () => {
                 </ImageContainer>
                 <TextInformation>
                     <Name>Zuo Fabian</Name>
-                    <Description>Je suis développeur</Description>
+                    <Description>Je suis <span ref={description}></span></Description>
                     <div>
-                        réseaux
+                        <UlSocial>
+                            <SocialElement className='icon_element'>
+                                <a href="#">
+                                    <FontAwesomeIcon className='icon' style={{padding:"5px", position:"absolute",top:"0",left:"0",width:"100%",height:"100%",display:"flex",justifyContent:"center",alignItems:"center",background: "#333",color:"rgba(255,255,255,0.2)"}} icon={faGithub} />
+                                </a>
+                            </SocialElement>
+                            <SocialElement>
+                                <a href="#">
+                                    <FontAwesomeIcon style={{padding:"5px", position:"absolute",top:"0",left:"0",width:"100%",height:"100%",display:"flex",justifyContent:"center",alignItems:"center",background: "#333",color:"rgba(255,255,255,0.2)",fontSize:"15px"}} icon={faTwitter} />
+                                </a>
+                            </SocialElement>
+                            <SocialElement>
+                                <a href="#">
+                                    <FontAwesomeIcon style={{padding:"5px",  position:"absolute",top:"0",left:"0",width:"100%",height:"100%",display:"flex",justifyContent:"center",alignItems:"center",background: "#333",color:"rgba(255,255,255,0.2)",fontSize:"15px"}} icon={faTwitter} />
+                                </a>
+                            </SocialElement>
+                        </UlSocial>
                     </div>
                 </TextInformation>
             </InformationContainer>
@@ -26,6 +65,7 @@ const Landing = () => {
 };
 const Container = styled.div`
     color:white;
+    padding:100px 0;
 `
 const Name = styled.h1`
     font-family: "Hazukaze";
@@ -45,12 +85,13 @@ const InformationContainer = styled.div`
     padding:0 50px;
 `
 const ImageContainer = styled.div`
-    width:50%;
+    width:65%;
     display:flex;
     justify-content:center;
     align-items:center;
 `
 const ImageBorder = styled.div`
+    width:40%;
     border:5px solid white;
     border-radius:50%;
     overflow:hidden;
@@ -61,7 +102,7 @@ const Image = styled.img`
     object-fit:cover;
 `
 const TextInformation = styled.div`
-    width:50%;
+    width:35%;
     display:flex;
     justify-content:center;
     flex-direction:column;
@@ -80,5 +121,44 @@ const Discover = styled.button`
     box-shadow: 0px 0px 13px 2px rgba(103,42,233,0.63);
     cursor:pointer;
 `
-
+const UlSocial = styled.ul`
+    margin-top:50px;
+    position:relative;
+    display:flex;
+    padding-left:15px;
+`
+const SocialElement = styled.li`
+    position:relative;
+    list-style:none;
+    width:40px;
+    height:35px;
+    margin-right:40px;
+    transform:rotate(-25deg) skew(25deg);
+    z-index:10;
+    &:before{
+        content:"";
+        position:absolute;
+        bottom:-10px;
+        left:0;
+        width:100%;
+        height:10px;
+        background:#2a2a2a;
+        transform-origin:top;
+        transform:skewX(-45deg);
+    }
+    &:after{
+        content:"";
+        position:absolute;
+        bottom:0;
+        left:-10px;
+        width:10px;
+        height:100%;
+        background:#2a2a2a;
+        transform-origin:right;
+        transform:skewY(-46deg);
+    }
+    /* &:hover{
+        background:red;
+    } */
+`
 export default Landing;
